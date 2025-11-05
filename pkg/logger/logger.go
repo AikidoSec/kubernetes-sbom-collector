2054954971ac7ab3b089836b3a0415466a5547db
NorbertHambuer <norbert@aikido.dev>
2025-11-05 15:58:21 +0200

    feat: Send complete error messages

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -41,8 +41,37 @@ func (s *Logger) ReportError(ctx context.Context, err error, message string, err
 
 	s.logger.Error(fmt.Sprintf("%s: %s", message, err.Error()), args...)
 
+	// Build error message as JSON
+	builder := strings.Builder{}
+	builder.WriteString("{\"message\":\"")
+	builder.WriteString(err.Error())
+	builder.WriteString("\"")
+
+	for i := 0; i < len(args)-1; i += 2 {
+		if i+1 >= len(args) {
+			break
+		}
+
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		builder.WriteString(",\"")
+		builder.WriteString(key)
+		builder.WriteString("\":")
+
+		argValue, err := json.Marshal(args[i+1])
+		if err != nil {
+			builder.WriteString(fmt.Sprintf(`"%v"`, args[i+1]))
+			continue
+		}
+		builder.WriteString(string(argValue))
+	}
+	builder.WriteString("}")
+	errorMessage := builder.String()
+
 	if err := s.sendError(ctx, models.AgentError{
-		Error:     fmt.Sprintf("%s: %s", message, err.Error()),
+		Error:     errorMessage,
 		ErrorType: errorType,
 		SeenAt:    time.Now().UTC(),
 	}); err != nil {
