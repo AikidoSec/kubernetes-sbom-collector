f827a75f0257fbdf38bbdc9f40ffb953d2999d42
NorbertHambuer <norbert@aikido.dev>
2026-04-29 14:01:34 +0300

    fix: Include pod source id in payload

--- pkg/models/sbom_payload.go
+++ pkg/models/sbom_payload.go
@@ -1,8 +1,9 @@
 package models
 
 type SBOMPayload struct {
-	Payload []byte `json:"payload"`
-	Image   string `json:"image"`
-	Digest  string `json:"digest"`
-	Tag     string `json:"tag"`
+	Payload     []byte `json:"payload"`
+	Image       string `json:"image"`
+	Digest      string `json:"digest"`
+	Tag         string `json:"tag"`
+	PodSourceID string `json:"pod_source_id"`
 }
