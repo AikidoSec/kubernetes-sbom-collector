6d4a1447b0ee4d8b6cfdd06f4a00c978fd6971be
NorbertHambuer <norbert@aikido.dev>
2026-07-29 16:48:21 +0300

    fix: Remove debugging code (again)

--- internal/controllers/watcher.go
+++ internal/controllers/watcher.go
@@ -101,9 +101,6 @@ func (r *Watcher) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result,
 	var processingErrors error
 	// We still process the images that were found even if there were errors listing some of them.
 	for _, img := range images {
-		if !strings.Contains(img.String(), "pause") {
-			continue
-		}
 		if img.Digest == "" {
 			r.Logger.ReportError(ctx, fmt.Errorf("%s", img.Name()), "image with empty SHA value", "sbomWatcherError", "pod", pod.Name, "namespace", pod.Namespace, "imageID", img.ResolvedImageID, "tag", img.Tag)
 			continue
@@ -168,12 +165,11 @@ func (r *Watcher) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result,
 			ImageUpdatedAt: imageSBOMResult.UpdatedAt,
 			AdditionalTags: imageSBOMResult.AdditionalTags,
 		}
-		fmt.Println(sbomPayload)
 
-		//if err := r.OperatorService.SendImageSBOM(ctx, sbomPayload); err != nil {
-		//	r.Logger.ReportError(ctx, err, "error sending SBOM payload", "sbomSendError", "pod", pod.Name, "namespace", pod.Namespace, "image", img.Name(), "sha", img.Digest, "tag", img.Tag)
-		//	processingErrors = multierror.Append(processingErrors, err)
-		//}
+		if err := r.OperatorService.SendImageSBOM(ctx, sbomPayload); err != nil {
+			r.Logger.ReportError(ctx, err, "error sending SBOM payload", "sbomSendError", "pod", pod.Name, "namespace", pod.Namespace, "image", img.Name(), "sha", img.Digest, "tag", img.Tag)
+			processingErrors = multierror.Append(processingErrors, err)
+		}
 	}
 
 	// If there were processing errors (either from checking the cache or from sending the SBOM), we return them so the controller can retry.
