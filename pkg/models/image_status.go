486b305cf9097e210955ad113a955b5fb3158e32
NorbertHambuer <norbert@aikido.dev>
2026-08-06 14:37:29 +0300

    feat: Deduplicate SBOM processing

--- pkg/models/image_status.go
+++ pkg/models/image_status.go
@@ -1,8 +1,9 @@
 package models
 
 type ImageStatus struct {
-	Image            string `json:"image"`
-	Digest           string `json:"digest"`
-	IsProcessed      bool   `json:"isProcessed"`
-	MirrorRepository string `json:"mirrorRepository"`
+	Image                              string `json:"image"`
+	Digest                             string `json:"digest"`
+	IsProcessed                        bool   `json:"isProcessed"`
+	MirrorRepository                   string `json:"mirrorRepository"`
+	IsBeingProcessedByAnotherCollector bool   `json:"isBeingProcessedByAnotherCollector"`
 }
